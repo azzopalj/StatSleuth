ce78b6e38a0cd12317a02828c79757a3f7343730
Lucas Azzopardi <azzopalj@gmail.com>
2026-06-02 22:01:18 -0400

    Initial Commit

---
+++ StatSleuth/StatSleuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatSleuthApp.swift
+//  StatSleuth
+//
+//  Created by Lucas Azzopardi on 2026-06-02.
+//
+
+import SwiftUI
+
+@main
+struct StatSleuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
